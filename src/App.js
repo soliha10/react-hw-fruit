9fda3819715731b613e1a18654b0a6b96b7dae73
soliha10 <toxtamurodovagulshodaa@gmail.com>
2024-08-06 18:17:58 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Children } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const {data} = this.props;
+    return (
+      <div>
+      {/* <h1>hi {this.props?.data?.title}</h1>
+      <h3>{this.props?.title} is js library</h3>
+      <h5>
+        {this.props?.data?.title } is {this.props?.data?.status || "unknown"}
+      </h5> */}
+      {/* {children} */}
+        
+      <li className="fruit-item">
+        <img className="fruit-pic" src={data.url}/>
+        {data.name}
+      </li>
+
+       
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Smth from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const students = [
+  { url: "https://img.freepik.com/free-vector/watermelon-fresh-fruit-icon-isolated-design_18591-82726.jpg", name: "Watermelon" },
+  { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVn7Squg2tb-nb8gbUyOqXvGcEHZlToz3ftQ&s", name: "Orange" },
+  { url: "https://img.freepik.com/free-vector/vibrant-red-cherry-isolated-vector_1308-133424.jpg", name: "Cherry" },
+  { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCyvfJIPkCc0LWU1H634sRUpf5W4P-BOZ_uQ&s", name: "Lemon " },
+  { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_uZXgDvpoo7BiAxsljjlUORt46-n5VviLRw&s", name: "Blueberry" },
+
+]
+
+
+ReactDOM.render(
+
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    {/* <Smth data={{ status:"active", title:"React" }}  />
+    <Smth title="Angular"/>
+    <Smth title="vue"/> */}
+    {/* <h2>Hi i'm a child</h2> */}
+    <h1 className="site-title">Fruits</h1>
+    <ul className="fruits-list">
+      {students.map((value) => {
+        return <Smth data={value} />
+      })}
+    </ul>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+  </React.StrictMode>,
+  document.getElementById("root")
+
+
+);
